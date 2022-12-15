b38e3f54fec67ea600bd6d589db7931c2993aba1
Shane Kitt <shane.kitt@protonmail.com>
2022-12-15 23:22:43 +0000

    Just another commit of my random noodling

--- testinggo.go
+++ testinggo.go
@@ -1,7 +1,10 @@
-package testinggo
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -9,4 +12,15 @@ func main() {
 	y := 3
 	z := x + y
 	fmt.Println(z)
+	myString := "This is a string\nAnother on a new line"
+	fmt.Println(myString)
+	for k, v := range myString {
+		fmt.Printf("k: %d,v: %c == %d\n", k, v, v)
+	}
+	s := strings.Join([]string{"hello", "world"}, ", ")
+	fmt.Println(s)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	fmt.Print("Your stupid name is " + name)
 }
